d22db87c3bdbe10fb012c3ee8f5f35bc4003e54c
Matt Jacobs <mattjacobs@Matts-MacBook-Pro.local>
2014-11-03 11:54:08 -0800

    ...

---
+++ mens-driving.php
@@ -0,0 +1,117 @@
+<?
+    $CatPage = true; 
+    $PageTitle = ' - Mens Driving Gloves';
+    $CatPage = 'driving';
+    $MainCat = 'mens';
+?>
+<?php include 'includes/pagetop_inc.php';?>
+
+<?php include 'includes/header_inc.php';?>
+<script type="text/javascript">var catPage = true;</script>
+
+<div class="content">
+    
+    <div class="sub-category-wrapper">
+            <div class="cat-info-wrapper">
+                <div class="cat-inner-wrapper">
+                   <div class="deer-dark"></div>
+                    <div class="cat-title"><img src="images/<?php echo $MainCat;?>-<?php echo $CatPage;?>-gloves-title.png"></div>
+                    <div class="lg-spacer"></div>
+                    <div class="lg-spacer"></div>
+                    <div class="lg-spacer"></div>
+                    <div class="box-info-title"><img src="images/<?php echo $MainCat;?>-<?php echo $CatPage;?>-info-title.png"></div>
+                    <div class="white-spacer"></div>
+                    <div class="box-info">
+                       Classic good looks that never go out of style<br/>and are built to last. This is a sample copy.
+                    </div>
+                </div>
+
+            </div>
+
+             <div class="cat-image <?php echo $MainCat;?>-<?php echo $CatPage;?>"></div>
+
+            <div class="clear"></div>
+
+            <div class="cat-links-wrapper">
+
+                <?php include 'includes/'.$MainCat.'_cats_inc.php';?>
+
+                <div class="clear"></div>
+            </div>
+              <div class="clear"></div>
+            <div class="gallery-title-wrapper">
+                <div class="gallery-line"></div>
+                <div class="gallery-title"><img src="images/<?php echo $MainCat;?>-<?php echo $CatPage;?>-title.png"></div>
+                <div class="gallery-line"></div>
+            </div>
+            <div class="clear"></div>
+            <div class="cat-gallery-wrapper">
+                
+                <div class="gallery-row">
+                    <div class="cat-item">
+                        <div class="cat-image-wrapper">
+                            <img src="images/temp-gloves.jpg">
+                        </div>
+                        <div class="cat-gallery-info">
+                            <div class="cat-item-title">Men's All-purpose Winter Gloves with SmartTek Lining</div>
+                            <div class="sub-title">ITM: 001</div>
+                        </div>
+                    </div>
+                    <div class="cat-item">
+                        <div class="cat-image-wrapper">
+                            <img src="images/temp-gloves.jpg">
+                        </div>
+                        <div class="cat-gallery-info">
+                            <div class="cat-item-title">Men's All-purpose Winter Gloves with SmartTek Lining</div>
+                            <div class="sub-title">ITM: 001</div>
+                        </div>
+                    </div>
+                    <div class="cat-item">
+                        <div class="cat-image-wrapper">
+                            <img src="images/temp-gloves.jpg">
+                        </div>
+                        <div class="cat-gallery-info">
+                            <div class="cat-item-title">Men's All-purpose Winter Gloves with SmartTek Lining</div>
+                            <div class="sub-title">ITM: 001</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="gallery-row">
+                    <div class="cat-item">
+                        <div class="cat-image-wrapper">
+                            <img src="images/temp-gloves.jpg">
+                        </div>
+                        <div class="cat-gallery-info">
+                            <div class="cat-item-title">Men's All-purpose Winter Gloves with SmartTek Lining</div>
+                            <div class="sub-title">ITM: 001</div>
+                        </div>
+                    </div>
+                    <div class="cat-item">
+                        <div class="cat-image-wrapper">
+                            <img src="images/temp-gloves.jpg">
+                        </div>
+                        <div class="cat-gallery-info">
+                            <div class="cat-item-title">Men's All-purpose Winter Gloves with SmartTek Lining</div>
+                            <div class="sub-title">ITM: 001</div>
+                        </div>
+                    </div>
+                    <div class="cat-item">
+                        <div class="cat-image-wrapper">
+                            <img src="images/temp-gloves.jpg">
+                        </div>
+                        <div class="cat-gallery-info">
+                            <div class="cat-item-title">Men's All-purpose Winter Gloves with SmartTek Lining</div>
+                            <div class="sub-title">ITM: 001</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="clear"></div>
+            
+    </div>  
+
+</div>
+
+<?php include 'includes/footer_inc.php';?>
+<?php include 'includes/pagebottom_inc.php';?>
